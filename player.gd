46b827c6043be1a6c69d54db0d47604f4cd2f977
omar_yahia <omaryk77@gmail.com>
2024-01-07 22:09:49 +0200

    shorter way to get direction using get_vector

--- player.gd
+++ player.gd
@@ -1,27 +1,14 @@
 extends CharacterBody2D
 var SPEED = 400
  
-func getX():
-	var direction=0
-	if Input.is_action_pressed('ui_left'):
-		direction=-1
-	elif Input.is_action_pressed('ui_right'):
-		direction=1
+func getDirection():
+	var direction=Input.get_vector("move_left","move_right","move_up","move_down")
 	return direction
-	
-func getY():
-	var direction=0
-	if Input.is_action_pressed('ui_up'):
-		direction=-1
-	elif Input.is_action_pressed('ui_down'):
-		direction=1
-	return direction
-	
+	 
 func movement(delta):
-	var x=getX()
-	var y=getY()
-	self.position.x+=x*SPEED*delta
-	self.position.y+=y*SPEED*delta
+	var direction=getDirection()
+	self.position+=direction*SPEED*delta
+ 
 
 func _physics_process(delta):
 	movement(delta)
