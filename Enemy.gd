b6a14ad4090c2ead9ed0942514378acbcd046173
omar_yahia <omaryk77@gmail.com>
2024-01-15 23:58:26 +0200

    simpler calculations for linear movement

--- Enemy.gd
+++ Enemy.gd
@@ -1,22 +1,12 @@
 extends CharacterBody2D
 class_name Enemy
 
-const SPEED = 50
+const SPEED = 150
+var direction
 func _ready():
-	#self.rotate(randi())
+	self.rotate(randi())
+	direction= Vector2.UP.rotated(self.rotation)
 	pass
 
-var dir=1
-var a=SPEED
 func _physics_process(delta):
-	a=SPEED*delta
-	if(randi() % 7 == 1):
-		dir*=-1
-		a*=10
-	else:
-		a=SPEED*delta
-		
-	
-	self.rotation+=delta*dir*sin(PI/23)
-	var direction = Vector2.UP.rotated(self.rotation)
-	self.position += direction * a
+	self.position += direction*SPEED*delta
